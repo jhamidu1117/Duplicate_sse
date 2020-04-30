52e6ff88d031d95841145c733cdc218633ea63a0
jhamidu1117 <hamidullah.mk1@gmail.com>
2020-04-30 15:01:06 -0500

    start of websocket integration with reddis

--- Duplicate_rest/urls.py
+++ Duplicate_rest/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_duplicates.urls')),
+    path('chat/', include('chat.urls'))
 ]
 
 
